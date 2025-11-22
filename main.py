4b51fc89a1386a79e36d71e0c9fe46c53355b455
Evactony <emma.anthony005@gmail.com>
2025-11-22 13:11:00 +0100

    To multiply numbers

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ def add_num(num1, num2):
 
 
 add_num(1, 2)
+
+
+def multiply_num(num1, num2):
+    product = num1 * num2
+    print("The product of the two numbers is: ", product)
+
+multiply_num(1, 2)
